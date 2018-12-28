496a975963e96b60b067ba1cc413c5f1c2872722
Damian <damian.rakus@gmail.com>
2018-12-28 16:14:39 +0100

    Cube is now rotating.

--- ARToolkit/app/src/main/java/com/rakusd/artoolkitproject/ARTRenderer.kt
+++ ARToolkit/app/src/main/java/com/rakusd/artoolkitproject/ARTRenderer.kt
@@ -1,5 +1,6 @@
 package com.rakusd.artoolkitproject
 
+import android.opengl.GLES10
 import android.opengl.GLES20
 
 import org.artoolkit.ar6.base.ARToolKit
@@ -12,24 +13,31 @@ import org.artoolkit.ar6.base.rendering.shader_impl.SimpleVertexShader
 
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
 
 class ARTRenderer : ARRenderer() {
 
+    lateinit var trackableUIDs:IntArray
+    lateinit var cube:Cube
+    var angle:Float=0f
 
     companion object {
         class Trackable(val name:String,val height:Float)
 
         val trackables:Array<Trackable> = arrayOf(
-            Trackable("Alterra_Ticket_1.jpg", 95.3f),
-            Trackable("Alterra_Postcard_2.jpg", 95.3f),
-            Trackable("Alterra_Postcard_3.jpg", 127.0f),
-            Trackable("Alterra_Postcard_4.jpg", 95.3f))
+            Trackable("Alterra_Ticket_1.jpg",95.3f))
+//        val trackables:Array<Trackable> = arrayOf(
+//            Trackable("Alterra_Ticket_1.jpg", 95.3f),
+//            Trackable("Alterra_Postcard_2.jpg", 95.3f),
+//            Trackable("Alterra_Postcard_3.jpg", 127.0f),
+//            Trackable("Alterra_Postcard_4.jpg", 95.3f))
 
 
 
     }
-    lateinit var trackableUIDs:IntArray
-    lateinit var cube:Cube
 
     override fun configureARScene(): Boolean {
         trackableUIDs= IntArray(trackables.size)
@@ -38,7 +46,7 @@ class ARTRenderer : ARRenderer() {
             if(trackableUIDs[i]<0)
                 return false
         }
-        NativeInterface.arwSetTrackerOptionInt(NativeInterface.ARW_TRACKER_OPTION_2D_MAX_IMAGES, trackables.size)
+        NativeInterface.arwSetTrackerOptionInt(NativeInterface.ARW_TRACKER_OPTION_2D_MAX_IMAGES, trackableUIDs.size)
         return true
     }
 
@@ -51,7 +59,11 @@ class ARTRenderer : ARRenderer() {
 
     override fun draw() {
         super.draw()
-
+        angle+=5
+        if(angle>360)
+            angle-=360
+        val sinus=sin(PI*(angle/180f)).toFloat()
+        val cosinus=cos(PI*(angle/180f)).toFloat()
         GLES20.glEnable(GLES20.GL_CULL_FACE)
         GLES20.glEnable(GLES20.GL_DEPTH_TEST)
         GLES20.glEnable(GLES20.GL_CCW)
@@ -60,7 +72,34 @@ class ARTRenderer : ARRenderer() {
             if(ARToolKit.getInstance().queryMarkerVisible(i)) {
                 var matrix:FloatArray=ARToolKit.getInstance().projectionMatrix
                 var modelMatrix:FloatArray=ARToolKit.getInstance().queryMarkerTransformation(i)
-                cube.draw(matrix,modelMatrix)
+
+                var model = FloatArray(16)
+                model[0]=modelMatrix[0]*cosinus+modelMatrix[1]*sinus
+                model[1]=modelMatrix[0]*(-sinus)+modelMatrix[1]*cosinus
+                model[2]=modelMatrix[2]
+                model[3]=modelMatrix[3]
+                model[4]=modelMatrix[4]*cosinus+modelMatrix[5]*sinus
+                model[5]=modelMatrix[4]*(-sinus)+modelMatrix[5]*cosinus
+                model[6]=modelMatrix[6]
+                model[7]=modelMatrix[7]
+                model[8]=modelMatrix[8]*cosinus+modelMatrix[9]*sinus
+                model[9]=modelMatrix[8]*(-sinus)+modelMatrix[9]*cosinus
+                model[10]=modelMatrix[10]
+                model[11]=modelMatrix[11]
+                model[12]=modelMatrix[12] //modelMatrix[12]*cosinus+modelMatrix[13]*sinus
+                model[13]=modelMatrix[13]//modelMatrix[12]*(-sinus)+modelMatrix[13]*cosinus
+                model[14]=modelMatrix[14]
+                model[15]=modelMatrix[15]
+
+
+                //modelMatrix[0]=cosinus
+                //modelMatrix[1]=-sinus
+                //modelMatrix[2]=0f
+                //modelMatrix[4]=sinus
+                //modelMatrix[5]=cosinus
+                //modelMatrix[6]=0f
+
+                cube.draw(matrix,model)
             }
         }
     }
